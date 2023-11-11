ee33aa4f16f8783be4e32ed460bc709f482279da
Ilyas Fauzal Akbar <ilyasbit01@gmail.com>
2023-11-12 04:35:27 +0700

    Update docker-entrypoint.sh

--- chia/harvester/docker-entrypoint.sh
+++ chia/harvester/docker-entrypoint.sh
@@ -19,3 +19,5 @@ chia configure --log-level INFO
 yq -i '.self_hostname = "0.0.0.0"' "$CHIA_ROOT/config/config.yaml"
 yq -i '.harvester.recursive_plot_scan = true' "$CHIA_ROOT/config/config.yaml"
 yq -i ".harvester.farmer_peer.host = \"$FARMER_HOST\"" "$CHIA_ROOT/config/config.yaml"
+
+exec "$@"
