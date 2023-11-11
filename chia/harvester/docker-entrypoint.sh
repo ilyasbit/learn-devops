01c2b4722c26051345cdfb27263c29cbb94448a7
Ilyas Fauzal Akbar <ilyasbit01@gmail.com>
2023-11-12 04:50:14 +0700

    Update docker-entrypoint.sh

--- chia/harvester/docker-entrypoint.sh
+++ chia/harvester/docker-entrypoint.sh
@@ -21,5 +21,6 @@ chia configure --log-level INFO
 yq -i '.self_hostname = "0.0.0.0"' "$CHIA_ROOT/config/config.yaml"
 yq -i '.harvester.recursive_plot_scan = true' "$CHIA_ROOT/config/config.yaml"
 yq -i ".harvester.farmer_peer.host = \"$FARMER_HOST\"" "$CHIA_ROOT/config/config.yaml"
+mkdir -p /storj-bucket
 chia plots add -d /storj-bucket
 exec "$@"
