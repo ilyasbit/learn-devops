20a965b69fc481d0d573db1ddc625ea31f14ae0e
Ilyas Fauzal Akbar <ilyasbit01@gmail.com>
2023-11-12 00:38:32 +0700

    change entrypoint setup

--- chia/farmerfull/docker-entrypoint.sh
+++ chia/farmerfull/docker-entrypoint.sh
@@ -39,5 +39,34 @@ rm -rf /tmp/seed.txt
 chia configure --log-level INFO
 
 yq -i '.self_hostname = "0.0.0.0"' "$CHIA_ROOT/config/config.yaml"
+yq -i '.harvester.recursi'
+
+chia start node wallet
+
+function syncAllWallet() {
+  while true; do
+    fullnodeSynced=$(chia rpc full_node get_blockchain_state | jq '.blockchain_state.sync.synced')
+    if [[ "$fullnodeSynced" == "true" ]]; then
+      break
+    fi
+    echo "Waiting for fullnode sync"
+    sleep 10
+  done
+  chia rpc wallet get_public_keys | jq -r '.public_key_fingerprints[]' | while read fingerprint; do
+    chia rpc wallet log_in "{ \"fingerprint\": $fingerprint }"
+    while true; do
+      walletSyncStatus=$(chia rpc wallet get_sync_status | jq -r '.synced')
+      if [[ "$walletSyncStatus" == "true" ]]; then
+        break
+        sleep 10
+      fi
+    done
+  done
+  firstWallet=$(chia rpc wallet get_public_keys | jq -r '.public_key_fingerprints[]' | head -n1)
+  chia rpc wallet log_in "{ \"fingerprint\": $firstWallet }"
+  echo "switched to first wallet"
+}
+
+syncAllWallet
 
 exec "$@"
